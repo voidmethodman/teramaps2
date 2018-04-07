4a9d40e68f4bfd2cb98155b4156d2cdc7e222845
hiroshi kotsuji <kotsjp@gmail.com>
2018-04-08 02:57:47 +0900

    create new project

---
+++ src/main/java/jp/or/shunjuen/teramaps2/Teramaps2Application.java
@@ -0,0 +1,12 @@
+package jp.or.shunjuen.teramaps2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teramaps2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teramaps2Application.class, args);
+	}
+}
---
+++ src/test/java/jp/or/shunjuen/teramaps2/Teramaps2ApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.or.shunjuen.teramaps2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Teramaps2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
